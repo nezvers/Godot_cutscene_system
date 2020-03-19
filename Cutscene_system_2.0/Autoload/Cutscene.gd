d745795435bc3f5ef0847cd6684c7875eb794c9b
nezvers <agnis16@inbox.lv>
2020-03-19 22:45:44 +0200

    Cleaned the system

--- Cutscene_system_2.0/Autoload/Cutscene.gd
+++ Cutscene_system_2.0/Autoload/Cutscene.gd
@@ -1,4 +1,65 @@
 extends Node
+
+class Pattern:
+	var pattern:Dictionary = {}
+	func _init()->void:
+		pass
+	func clear()->void:
+		pattern.clear()
+	func start()->void:
+		if !Cutscene.pattern.empty():			#check if cutscene is busy
+			return
+		Cutscene.start(pattern)
+#************** Add to pattern functions ***************
+	func add_wait(duration:float)->void:
+		pattern[pattern.size()] = {
+			'cutscene_method' : "wait",
+			'cutscene_arguments' : duration
+		}
+	func add_call_method(object:Object, method:String, parameters:Array)->void:
+		pattern[pattern.size()] = {
+			'cutscene_method' : "call_method",
+			'cutscene_arguments' : [object, method, parameters ]
+		}
+	func add_multi_call_method(method_list:Array)->void:
+		pattern[pattern.size()] = {
+			'cutscene_method' : "multi_call_method",
+			'cutscene_arguments' : method_list
+		}
+	func add_interpolate_value(object: Object, property: NodePath, initial_val, final_val, duration: float, trans_type: int = 0, ease_type: int = 2, delay: float = 0)->void:
+		pattern[pattern.size()] = {
+			'cutscene_method' : "interpolate_value",
+			'cutscene_arguments' : [object, property, initial_val, final_val, duration, trans_type, ease_type, delay]
+		}
+	func add_multi_interpolate_value(interpolate_list:Array)->void:
+		pattern[pattern.size()] = {
+			'cutscene_method' : "multi_interpolate_value",
+			'cutscene_arguments' : interpolate_list
+		}
+	func add_move(relative:bool, global:bool, object:Object, position:Vector2, duration:float, delay:float)->void:
+		pattern[pattern.size()] = {
+			'cutscene_method' : "move",
+			'cutscene_arguments' : [relative, global, object, position, duration, delay]
+		}
+	func add_multi_move(move_list:Array)->void:
+		pattern[pattern.size()] = {
+			'cutscene_method' : "move",
+			'cutscene_arguments' : move_list
+		}
+	#Create an object that needs to be instanced
+	func add_create_object(object: Object, parent:Node, global_pos: Vector2)->void:
+		pattern[pattern.size()] = {
+			'cutscene_method' : "move",
+			'cutscene_arguments' : [object, parent, global_pos]
+		}
+	#Functions for multi versions
+	func single_method(object:Object, method:String, parameters:Array)->Array:
+		return [object, method, parameters]
+	func single_interpolate_value(object: Object, property: NodePath, initial_val, final_val, duration: float, trans_type: int = 0, ease_type: int = 2, delay: float = 0)->Array:
+		return[object, property, initial_val, final_val, duration, trans_type, ease_type, delay]
+	func single_move(relative:bool, global:bool, object:Object, position:Vector2, duration:float, delay:float)->Array:
+		return [relative, global, object, position]
+
 signal cutscene_started
 signal cutscene_ended
 
--- Cutscene_system_2.0/Objects/CutsceneTrigger.gd
+++ Cutscene_system_2.0/Objects/CutsceneTrigger.gd
@@ -1,74 +1,48 @@
 extends Area2D
 class_name CutsceneTrigger
 
-var triggering_object: Object = null		#will be used to reference object
-
-var pattern:Dictionary = {}
-var order: Array = []
+var triggering_object: Object = null		#will be used to reference player object
+var pattern = Cutscene.Pattern.new()		#New instance of cutscene pattern object
 
 func _ready()->void:
 	connect("body_entered", self, "_on_Trigger_body_entered")
 
-#Virtual function where to add pattern list
+#Create pattern list
 func set_pattern()->void:
-	pass
+	pattern.add_call_method(triggering_object, "set", ["can_move", false])
+	pattern.add_wait(0.2)
+	pattern.add_call_method(triggering_object, "set", ["position", Vector2(1280/2, 720/2)])
+	pattern.add_wait(0.2)
+	pattern.add_call_method(triggering_object, "set", ["modulate", Color(randf(), randf(), randf(), 1.0)])
+	pattern.add_wait(0.2)
+	pattern.add_call_method(triggering_object, "set", ["scale", Vector2(2,3)])
+	pattern.add_wait(0.2)
+	pattern.add_multi_call_method([				#Array
+		pattern.single_method(triggering_object, "set", ["position", Vector2(1280 * 0.75, 720 * 0.25)]),
+		pattern.single_method(triggering_object, "set", ["modulate", Color(randf(), randf(), randf(), 1.0)]),
+		pattern.single_method(triggering_object, "set", ["scale", Vector2(2,3)])
+	])
+	pattern.add_wait(0.2)
+	pattern.add_interpolate_value(triggering_object, "modulate", Color.white, Color.blue, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0)
+	pattern.add_wait(0.2)
+	pattern.add_multi_interpolate_value([		#Array
+		pattern.single_interpolate_value(triggering_object, "modulate", Color.white, Color.blue, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0),
+		pattern.single_interpolate_value(triggering_object, "position", triggering_object.position, Vector2(800,600), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0.3)
+	])
+	pattern.add_wait(0.2)
+	pattern.add_move(true, true, triggering_object, Vector2(-200, -100), 1, 0)
+	pattern.add_call_method(triggering_object, "set", ["scale", Vector2(1,1)])
+	pattern.add_call_method(triggering_object, "set", ["modulate", Color(1, 1, 1, 1)])
+	pattern.add_wait(0.2)
+	pattern.add_call_method(triggering_object, "set", ["can_move", true])
 
 func _on_Trigger_body_entered(body):
-	if !Cutscene.pattern.empty():			#check if cutscene is busy
-		return
+	
 	triggering_object = body
 	pattern.clear()	#clear previous pattern list
 	set_pattern()
-	Cutscene.start(pattern)
+	pattern.start()	#starts only if no active cutscene pattern
 
 
-#************** Add to pattern functions ***************
-func add_wait(duration:float)->void:
-	pattern[pattern.size()] = {
-		'cutscene_method' : "wait",
-		'cutscene_arguments' : duration
-	}
-func add_call_method(object:Object, method:String, parameters:Array)->void:
-	pattern[pattern.size()] = {
-		'cutscene_method' : "call_method",
-		'cutscene_arguments' : [object, method, parameters ]
-	}
-func add_multi_call_method(method_list:Array)->void:
-	pattern[pattern.size()] = {
-		'cutscene_method' : "multi_call_method",
-		'cutscene_arguments' : method_list
-	}
-func add_interpolate_value(object: Object, property: NodePath, initial_val, final_val, duration: float, trans_type: int = 0, ease_type: int = 2, delay: float = 0)->void:
-	pattern[pattern.size()] = {
-		'cutscene_method' : "interpolate_value",
-		'cutscene_arguments' : [object, property, initial_val, final_val, duration, trans_type, ease_type, delay]
-	}
-func add_multi_interpolate_value(interpolate_list:Array)->void:
-	pattern[pattern.size()] = {
-		'cutscene_method' : "multi_interpolate_value",
-		'cutscene_arguments' : interpolate_list
-	}
-func add_move(relative:bool, global:bool, object:Object, position:Vector2, duration:float, delay:float)->void:
-	pattern[pattern.size()] = {
-		'cutscene_method' : "move",
-		'cutscene_arguments' : [relative, global, object, position, duration, delay]
-	}
-func add_multi_move(move_list:Array)->void:
-	pattern[pattern.size()] = {
-		'cutscene_method' : "move",
-		'cutscene_arguments' : move_list
-	}
-#Create an object that needs to be instanced
-func add_create_object(object: Object, parent:Node, global_pos: Vector2)->void:
-	pattern[pattern.size()] = {
-		'cutscene_method' : "move",
-		'cutscene_arguments' : [object, parent, global_pos]
-	}
-#Functions for multi versions
-func single_method(object:Object, method:String, parameters:Array)->Array:
-	return [object, method, parameters]
-func single_interpolate_value(object: Object, property: NodePath, initial_val, final_val, duration: float, trans_type: int = 0, ease_type: int = 2, delay: float = 0)->Array:
-	return[object, property, initial_val, final_val, duration, trans_type, ease_type, delay]
-func single_move(relative:bool, global:bool, object:Object, position:Vector2, duration:float, delay:float)->Array:
-	return [relative, global, object, position]
+
 
